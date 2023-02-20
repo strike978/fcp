87199c87308859a2c9dd18368afc86bb46495611
Omar Nunez <onunez978@gmail.com>
2023-02-19 23:22:17 -0500

    changed application

--- st.py
+++ st.py
@@ -6,11 +6,10 @@ import streamlit as st
 # Set page title
 st.set_page_config(page_title="Find Closest Populations", page_icon="🌎")
 
-# Add page header
-st.caption("This web application leverages G25 population data to calculate the distances between selected target populations and a reference population. The distances are calculated using the Euclidean algorithm, a popular method for measuring the geometric distance between two points in a high-dimensional space.")
-
 # Read in data from the selected input file
 t = pd.read_csv('Modern Era.txt', delimiter=",", index_col=0)
+
+st.caption("Calculate similarity between populations using G25 population data and the Euclidean algorithm. Choose to enter G25 coordinates manually or select a population.")
 
 # Add a checkbox for selecting input data from the input file
 use_manual_input = st.checkbox("Use G25 Coordinates")
@@ -26,31 +25,30 @@ if use_manual_input:
     else:
         input_row = None
 else:
-    input_row_name = st.selectbox("Select reference population", t.index)
+    input_row_name = st.selectbox("Select population", t.index)
     input_row = t.loc[input_row_name]
 
-# Define a multiselect box for the target rows
-target_rows = st.multiselect("Select target populations", t.index)
-
-if not target_rows:
-    st.error("Please select at least one target population.")
+# Define a slider for the number of rows to display
+num_rows = st.slider("Number of populations to display",
+                     min_value=1, max_value=len(t.index), value=10)
 
 # Add a "Calculate Distance" button
-if st.button("Calculate Distance"):
+if st.button("Calculate") and input_row is not None:
 
-  # This is the code that calculates the distance between the selected populations.
-    distances = {}
-    if input_row is not None:
-        for target_row in target_rows:
+    # Calculate distances between the reference population and all other populations
+    similarities = {}
+    for target_row in t.index:
+        if target_row != input_row_name:
             d = squareform(pdist(np.vstack((input_row, t.loc[target_row]))))
             distance = d[0, 1]
-            distances[target_row] = distance
+            similarity = (1 - distance) * 100
+            similarities[target_row] = similarity
 
-        sorted_target_rows = sorted(distances, key=distances.get)
-        closest_target_row = sorted_target_rows[0]
-        closest_distance = distances[closest_target_row]
+    sorted_target_rows = sorted(
+        similarities, key=similarities.get, reverse=True)
+    most_similar_target_row = sorted_target_rows[0]
+    most_similar_similarity = similarities[most_similar_target_row]
 
-  # Printing the distance between the selected populations.
-        st.write("Distance to:")
-        for target_row in sorted_target_rows:
-            st.code(f"{target_row}: {distances[target_row]:.2f}")
+    # Printing the similarity index between the reference population and all other populations
+    for i, target_row in enumerate(sorted_target_rows[:num_rows]):
+        st.code(f"{i + 1}. {target_row}: {similarities[target_row]:.2f}%")
